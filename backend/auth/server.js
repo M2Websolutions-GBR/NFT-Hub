238fb63744640c6ff393c2c31ba5402091ecd5de
Jumpdaddy78 <hackcheck78@gmail.com>
2025-07-22 12:40:12 +0200

    added dockerfile for auth service and compose for DB connetion

--- backend/auth/server.js
+++ backend/auth/server.js
@@ -2,11 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js';
-
+import DBconnection from './config/db.js';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+
+
+DBconnection();
 
 app.use(cors());
 app.use(express.json());
