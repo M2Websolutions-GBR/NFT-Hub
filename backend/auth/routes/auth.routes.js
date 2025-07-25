ed8f16026af7f0c5fd4f04d3803f25ab34a273d7
GitZillaa <marcmc.eh@gmail.com>
2025-07-25 10:36:02 +0200

    profile route in auth service and readme

--- backend/auth/routes/auth.routes.js
+++ backend/auth/routes/auth.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { register, login } from '../controllers/auth.controller.js';
+import { register, login, getUserById } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/user/:id', getUserById); 
+
 
 export default router;
 
